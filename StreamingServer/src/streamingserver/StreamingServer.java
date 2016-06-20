7bff0ebe254246f5e36fd09854abf2ede26d824e
RafaelMonteiro95 <rafaelmonteiro95@gmail.com>
2016-06-19 21:39:03 -0300

    Add files via upload

---
+++ StreamingServer/src/streamingserver/StreamingServer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package streamingserver;
+
+/**
+ *
+ * @author Rafael Augusto Monteiro - 9293095
+ */
+public class StreamingServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
